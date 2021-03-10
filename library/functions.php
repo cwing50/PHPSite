2c33a559b76491cb77db453b0882ce2cbaef40f8
Caden Wingler <cwingler4@bcst01.parkland.edu>
2021-03-10 14:15:13 -0600

    Added function for printing set name in header

--- library/functions.php
+++ library/functions.php
@@ -1,8 +1,22 @@
 <?php
 //functions
+function printcookiename()
+{
+	if ( isset($_COOKIE['personname']))
+	{
+		echo "<h3>Welcome $_COOKIE[personname]!</h3>";
+	}
+}
+
+
+
 if ( isset($_POST["logout"]) )
 {
 	header("Location: logout.php");
+}
+if ( isset($_POST["main"]) )
+{
+	header("Location: main.php");
 }
 if ( isset($_POST["item1"]) )
 {
@@ -29,10 +43,13 @@ function footer()
 {
 	echo "<form method='POST'>";
 	echo "<input type='submit' name='logout' value='Logout'>";
-	echo "<input type='submit' name='item1' value='Item1'>";
-	echo "<input type='submit' name='item2' value='Item2'>";
-	echo "<input type='submit' name='item3' value='Item3'>";
-	echo "<input type='submit' name='item4' value='Item4'>";
+	echo "<input type='submit' name='main' value='Main'>";
+	echo "  <b>Cars:</b>  ";
+	echo "<input type='submit' name='item1' value='2020 Mustang GT500'>";
+	echo "<input type='submit' name='item2' value='2020 Camaro ZL1'>";
+	echo "<input type='submit' name='item3' value='2020 Dodge Challenger Hellcat'>";
+	echo "<input type='submit' name='item4' value='2020 Dodge Charger Hellcat'>";
+	echo "    ";
 	echo "<input type='submit' name='cart' value='Cart'>";
 	echo "</form>";
 }
@@ -41,7 +58,8 @@ function heading()
 {
 echo "<p>Caden Wingler</p>";
 echo "<p>CSC-155-051DV_2021SP</p>";
-echo "<img src='library/avatar.jpg' height=100 width=100/>";
+echo "<img src='library/images/avatar.jpg' height=100 width=100/>";
+printcookiename();
 }
 
 function startup()
